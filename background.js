dd83670ae5828b279fbfbdb7978963c086961c9d
Huong Nguyen <shovity@gmail.com>
2017-12-27 22:53:08 +0700

    add bookmark bar

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// REQUEST LIST
+// REQUEST LIST 
 const GET_BOOkMARK = 'GET_BOOkMARK'
 const GET_MOSTSITE = 'GET_MOSTSITE'
 
@@ -7,12 +7,39 @@ chrome.runtime.onConnect.addListener(function(port) {
   // Only service port name "pip"
   if (port.name !== 'pip') return
 
+
+  // listen update bookmark
+  chrome.bookmarks.onCreated.addListener(() => {
+    chrome.bookmarks.getTree((bookmarkNodes) => {
+      port.postMessage({ request: GET_BOOkMARK, data: bookmarkNodes })
+    })
+  })
+
+  chrome.bookmarks.onRemoved.addListener(() => {
+    chrome.bookmarks.getTree((bookmarkNodes) => {
+      port.postMessage({ request: GET_BOOkMARK, data: bookmarkNodes })
+    })
+  })
+
+  chrome.bookmarks.onChanged.addListener(() => {
+    chrome.bookmarks.getTree((bookmarkNodes) => {
+      port.postMessage({ request: GET_BOOkMARK, data: bookmarkNodes })
+    })
+  })
+
+  chrome.bookmarks.onMoved.addListener(() => {
+    chrome.bookmarks.getTree((bookmarkNodes) => {
+      port.postMessage({ request: GET_BOOkMARK, data: bookmarkNodes })
+    })
+  })
+
+  // listen mesg from newtab page
   port.onMessage.addListener(function(msg) {
     switch (msg.request) {
 
       case GET_BOOkMARK:
         // Get bookmarks recent
-        chrome.bookmarks.getRecent(7, (bookmarkNodes) => {
+        chrome.bookmarks.getTree((bookmarkNodes) => {
           port.postMessage({ request: msg.request, data: bookmarkNodes })
         })
         break
--- statics/javascripts/newtab.js
+++ statics/javascripts/newtab.js
@@ -6,12 +6,61 @@ const GET_MOSTSITE = 'GET_MOSTSITE'
 const wallpaperRandom = false
 
 // Init varible
-const images = window.imageIndex.map(e => `statics/images/${e}`)
+const images = window.imageIndex.map(e => `statics/images/backgrounds/${e}`)
 
 // Random wallpaper
 if (wallpaperRandom) {
   const i = Math.floor(Math.random() * images.length)
   window.document.body.style.backgroundImage = `url(${images[i]})`
+}
+
+/**
+ * Book mark bar handle
+ */
+const bookmark = {}
+
+bookmark.htmlElement = bookmarkBar
+
+bookmark.createItem = (node) => {
+  const { url, title, children } = node
+  if (children) {
+    setTimeout(() => {
+      bookmark.render(node)
+    })
+    return ''
+  }
+
+  return (
+    `<a class="item" href="${url}">
+      <img src="chrome://favicon/${url}">
+      <div class="title">${title}</div>
+    </a>`
+  )
+}
+
+bookmark.createParent = (node) => {
+  const label = node.title
+  const nodes = node.children
+  
+  let result = '<div class="parent">'
+
+  result += `
+  <div class="parent-header">
+    <img src="/statics/images/folder.svg">
+    <div class="label">${label}</div>
+  </div>`
+
+  nodes.forEach(node => {
+    result += bookmark.createItem(node)
+  })
+
+  return result + '</div>'
+}
+
+bookmark.render = (node, clear = false) => {
+  if (clear) bookmark.htmlElement.innerHTML = ''
+  bookmark.htmlElement.innerHTML += bookmark.createParent(node)
+  return ''
 }
 
 /**
@@ -30,14 +79,16 @@ port.onMessage.addListener(({ request, data, err }) => {
   switch (request) {
 
     case GET_BOOkMARK:
-      console.log(data)
+      bookmark.render(data[0].children[0], true)
       break
 
     case GET_MOSTSITE:
-      console.log(data)
+      // console.log(data)
       break
 
     default:
       console.log('Revice response not match')
   }
 })
+
+
